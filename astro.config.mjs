ee99734cdf3c95e745ab018025d7b27836fa7137
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2024-11-30 07:27:33 +0530

    Added server side rendering. Removed zombie code.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: 'hybrid',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
