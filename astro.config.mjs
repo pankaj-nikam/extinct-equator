3b34d42416c78ea757b0d7558056f7ddd94700a4
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2024-12-02 08:31:27 +0530

    Added react. Refactored the validation functions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import mdx from '@astrojs/mdx';
 
 import node from '@astrojs/node';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   output: 'hybrid',
 
   adapter: node({
